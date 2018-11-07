1e5943df2da1c9ba209d6eb8b3d3a364acec78b9
jatisto <j.atisto@gmail.com>
2018-11-07 12:08:27 +0600

    Change@RenderBody_Layout
    Сделал 3 дива и в середину вставил @RenderBody для создания меню

--- AutoTotalMind/AutoTotalMind/Controllers/HomeController.cs
+++ AutoTotalMind/AutoTotalMind/Controllers/HomeController.cs
@@ -13,8 +13,9 @@ namespace AutoTotalMind.Controllers
 
         public ActionResult Index()
         {
-            ViewBag.RandomPictures = _context.ImageFactory.TakeRandom(3);
-            return View(_context.SubpageFactory.Get(3));
+            ViewBag.RandomPictures = _context.ImageFactory.TakeRandom(3); // рандомные картинки
+            _context.ProductFactory.CountBy("BrandID", 1);
+            return View(_context.SubpageFactory.Get(3)); // вывести subpage с ID 3
         }
     }
 }
